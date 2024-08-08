a8bd93ee2ee08b080ca93a51112d01c886f3192c
Phạm Huy <104686007+HueyPixelBoxes@users.noreply.github.com>
2024-08-08 22:24:47 +0700

    Add Oauth2 for Google and login in Use huyphamquang2003@gmail.com as support dev email https://developers.facebook.com/apps/850711396649691/dashboard/ and https://console.cloud.google.com/apis/credentials?project=top-script-406805
    for client and app id and sercets
    change dependency and update numbus to other version due to secirity reason

--- src/main/java/com/group12/springboot/hoversprite/config/SecurityConfig.java
+++ src/main/java/com/group12/springboot/hoversprite/config/SecurityConfig.java
@@ -1,15 +1,15 @@
 package com.group12.springboot.hoversprite.config;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-
+@Order(SecurityProperties.BASIC_AUTH_ORDER)
 public class SecurityConfig{
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
