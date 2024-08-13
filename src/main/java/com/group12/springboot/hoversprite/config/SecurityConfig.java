20700e0eeab0d102a81ce03d79fc4ef281456f2a
Phạm Huy <104686007+HueyPixelBoxes@users.noreply.github.com>
2024-08-13 09:36:52 +0700

    Add Oauth2 for Google and login in Use huyphamquang2003@gmail.com as support dev email https://developers.facebook.com/apps/850711396649691/dashboard/ and https://console.cloud.google.com/apis/credentials?project=top-script-406805
    for client and app id and sercets
    change dependency and update numbus to other version due to secirity reason

--- src/main/java/com/group12/springboot/hoversprite/config/SecurityConfig.java
+++ src/main/java/com/group12/springboot/hoversprite/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.group12.springboot.hoversprite.config;
 
+import com.group12.springboot.hoversprite.exception.CustomAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,11 +16,10 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.group12.springboot.hoversprite.exception.CustomAuthenticationEntryPoint;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@Order(SecurityProperties.BASIC_AUTH_ORDER)
 public class SecurityConfig {
     private final String[] PUBLIC_ENDPOINT = {"/register/farmer", "/auth/login", "/auth/introspect", "/auth/logout"};
 
@@ -35,6 +38,17 @@ public class SecurityConfig {
                                                                                             
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
+    }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests( auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 
     @Bean
