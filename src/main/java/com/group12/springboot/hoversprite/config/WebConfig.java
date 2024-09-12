e70aeb99158642a21b2ec406c469ba12b3a07433
LtMtrunG <s3927071@rmit.edu.vn>
2024-09-12 17:14:57 +0700

    Api documentation

--- src/main/java/com/group12/springboot/hoversprite/config/WebConfig.java
+++ src/main/java/com/group12/springboot/hoversprite/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.group12.springboot.hoversprite.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -22,6 +23,23 @@ public class WebConfig implements WebMvcConfigurer {
 //            }
 //        };
 //    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Serve static resources from /static/
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+
+        // Serve Swagger UI at /swagger-ui/index.html
+        registry.addResourceHandler("/swagger-ui/**").addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/");
+
+        // Serve webjars (used by Swagger for additional dependencies)
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        // For older Swagger versions, you might need this line (for swagger-ui.html):
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+//        super.addResourceHandlers(registry);
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
