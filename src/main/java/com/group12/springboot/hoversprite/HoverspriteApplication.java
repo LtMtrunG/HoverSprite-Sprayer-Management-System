fdceef047a2c7070ca7a0beb7f0c26da55a662b8
TMT <105305209+LtMtrunG@users.noreply.github.com>
2024-10-04 03:10:47 +0700

    add keystore classpath

--- src/main/java/com/group12/springboot/hoversprite/HoverspriteApplication.java
+++ src/main/java/com/group12/springboot/hoversprite/HoverspriteApplication.java
@@ -2,7 +2,9 @@ package com.group12.springboot.hoversprite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }, scanBasePackages = {"repository","controller","service","entity"})
 @SpringBootApplication
 public class HoverspriteApplication {
 
--- src/main/java/com/group12/springboot/hoversprite/feedback/repository/FeedbackRepository.java
+++ src/main/java/com/group12/springboot/hoversprite/feedback/repository/FeedbackRepository.java
@@ -2,9 +2,11 @@ package com.group12.springboot.hoversprite.feedback.repository;
 
 import com.group12.springboot.hoversprite.feedback.entity.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
     Optional<Feedback> findById(Long id);
 }
